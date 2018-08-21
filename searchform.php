88c18d8754b15be82b6b6ddd90e694cd56c78149
Emmanuel ARNOUD <emmanuel.arnoud@gmail.com>
2018-08-21 12:57:54 +0200

    Assistive-text added to searchform.php
    using BS4 class sr-only for screen reader text.

--- searchform.php
+++ searchform.php
@@ -1,13 +1,12 @@
 <?php
 /**
  * The template for displaying search forms in Underscores.me
- * Customized for Pinegrow ST2
- * Removed <label class="assistive-text" for="s"><?php esc_html_e( 'Search', 'st2' ); ?></label>
- */
+
+**/
 
 ?>
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-
+	<label class="sr-only" for="s"><?php esc_html_e( 'Search', 'st2' ); ?></label>
 	<div class="input-group">
 		<input class="field form-control" id="s" name="s" type="text"
 			placeholder="<?php esc_attr_e( 'Search &hellip;', 'st2' ); ?>" value="<?php the_search_query(); ?>">
